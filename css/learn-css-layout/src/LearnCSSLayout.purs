b6a9cba5b7732e7d9a3abb6e1ef12ededcc6427b
boygao1992 <wg2313@columbia.edu>
2018-10-14 21:05:39 -0400

    modular css: replaced inline CSS by classNames

--- css/learn-css-layout/src/LearnCSSLayout.purs
+++ css/learn-css-layout/src/LearnCSSLayout.purs
@@ -2,22 +2,27 @@ module LearnCSSLayout where
 
 import Prelude
 
+import Data.Array (filter)
+import Data.Tuple (Tuple(..), fst, snd)
+import CSS.Selector (element) as CSS
+import CSS.Stylesheet (CSS)
+import CSS.Stylesheet (select) as CSS
 import CSS.Background (backgroundColor)
-import CSS.Border (solid, border) as CB
-import CSS.Common (auto) as CC
-import CSS.Display (position, absolute, relative) as CD
-import CSS.Font (color) as CF
-import CSS.Geometry (width, maxWidth, margin, top, left, padding, lineHeight) as CG
-import CSS.Size as CS
-import CSS.Stylesheet (StyleM)
+import CSS.Border (solid, border) as CSS
+import CSS.Common (auto) as CSS
+import CSS.Display (position, absolute, relative) as CSS
+import CSS.Font (color) as CSS
+import CSS.Geometry (width, maxWidth, margin, top, left, padding, lineHeight) as CSS
+import CSS.Size (nil, px, em) as CSS
 import Color (Color)
 import Color as Color
 import Data.Maybe (Maybe(..), fromMaybe)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.CSS as HC
--- import Halogen.HTML.Properties as HP
+import Halogen.HTML.Properties as HP
 
+-- | Types
 type State = Unit
 initialState :: State
 initialState = unit
@@ -29,6 +34,7 @@ type Output = Void
 
 -- type IO = Aff
 
+-- | Color
 bgColor :: Color
 bgColor = (fromMaybe Color.white) <<< Color.fromHexString $ "#ECECEC"
 
@@ -38,54 +44,69 @@ color_green = (fromMaybe Color.white) <<< Color.fromHexString $ "#6AC5AC"
 color_dark :: Color
 color_dark = (fromMaybe Color.white) <<< Color.fromHexString $ "#414142"
 
-style_main :: StyleM Unit
+-- | CSS
+style_main :: CSS
 style_main = do
-  CG.width $ CS.px 600.0
-  CG.margin CS.nil CC.auto CS.nil CC.auto
+  CSS.width $ CSS.px 600.0
+  CSS.margin CSS.nil CSS.auto CSS.nil CSS.auto
 
-style_main2 :: StyleM Unit
+style_main2 :: CSS
 style_main2 = do
-  CG.maxWidth $ CS.px 600.0
-  CG.margin CS.nil CC.auto CS.nil CC.auto
+  CSS.maxWidth $ CSS.px 600.0
+  CSS.margin CSS.nil CSS.auto CSS.nil CSS.auto
 
-style_elem :: StyleM Unit
+style_elem :: CSS
 style_elem = do
-  CB.border CB.solid (CS.px 3.0) color_green
-  CD.position CD.relative
+  CSS.border CSS.solid (CSS.px 3.0) color_green
+  CSS.position CSS.relative
 
-style_code :: StyleM Unit
+style_code :: CSS
 style_code = backgroundColor bgColor
 
-style_label :: StyleM Unit
+style_label :: CSS
 style_label = do
-  CG.top CS.nil
-  CG.left CS.nil
-  CG.padding CS.nil (CS.px 3.0) (CS.px 3.0) CS.nil
+  CSS.top CSS.nil
+  CSS.left CSS.nil
+  CSS.padding CSS.nil (CSS.px 3.0) (CSS.px 3.0) CSS.nil
 
-style_endlabel :: StyleM Unit
+style_endlabel :: CSS
 style_endlabel = do
   style_label
-  CD.position CD.absolute
+  CSS.position CSS.absolute
   backgroundColor color_green
-  CF.color color_dark
-  CG.lineHeight $ CS.em 1.0
+  CSS.color color_dark
+  CSS.lineHeight $ CSS.em 1.0
 
+style_select :: CSS
+style_select = do
+  CSS.select (CSS.element "code") style_code
+  CSS.select (CSS.element ".main") style_main
+  CSS.select (CSS.element ".elem") style_elem
+  CSS.select (CSS.element ".label") style_endlabel
+  CSS.select (CSS.element ".main2") style_main2
+
+-- | Utils
+classList
+  :: forall r i
+   . Array (Tuple String Boolean)
+  -> H.IProp ("class" :: String | r) i
+classList = HP.classes <<< map (H.ClassName <<< fst) <<< filter snd
+
+-- | HTML
 marginAuto :: forall q. H.ComponentHTML q
 marginAuto =
-  HH.div [ HC.style do
-             style_main
-             style_elem
+  HH.div [ classList [ Tuple "main" true
+                     , Tuple "elem" true
+                     ]
          ]
-  [ HH.span [ HC.style style_endlabel]
+  [ HH.span [ HP.class_ $ H.ClassName "label"]
     [ HH.text "<div id=\"main\">"]
   , HH.p_
     [ HH.text "Setting the "
-    , HH.code
-        [ HC.style style_code ]
+    , HH.code_
         [ HH.text "width" ]
     , HH.text " of a block-level element will prevent it from stretching out to the edges of its container to the left and right. Then, you can set the left and right margins to "
-    , HH.code
-        [ HC.style style_code ]
+    , HH.code_
         [ HH.text "auto"]
     , HH.text " to horizontally center that element within its container. The element will take up the width you specify, then the remaining space will be split evenly between the two margins."
     ]
@@ -93,11 +114,41 @@ marginAuto =
     [ HH.text "The only problem occurs when the browser window is narrower than the width of your element. The browser resolves this by creating a horizontal scrollbar on the page. Let's improve the situation..."]
   ]
 
+maxWidth :: forall q. H.ComponentHTML q
+maxWidth =
+  HH.div [ classList [ Tuple "main2" true
+                     , Tuple "elem" true
+                     ]
+         ]
+  [ HH.span [ HP.class_ $ H.ClassName "label"]
+    [ HH.text "<div id=\"main2\">"]
+  , HH.p_
+    [ HH.text "Using "
+    , HH.code_
+        [ HH.text "max-width" ]
+    , HH.text " instead of "
+    , HH.code_
+        [ HH.text "width"]
+    , HH.text " in this situation will improve the browser's handling of small windows. This is important when making a site usable on mobile. Resize this page to check it out!"
+    ]
+  , HH.p_
+    [ HH.text "By the way, "
+    , HH.code_
+        [ HH.text "max-width" ]
+    , HH.text " is "
+    , HH.a [ HP.href "http://caniuse.com/#search=max-width"]
+      [ HH.text "supported by all major browsers"]
+    , HH.text " including IE7+ so you shouldn't be afraid of using it."
+    ]
+  ]
 
+-- | Component
 render :: State -> H.ComponentHTML Query
 render _ =
   HH.div_
   [ marginAuto
+  , maxWidth
+  , HC.stylesheet style_select
   ]
 
 eval :: forall m. Query ~> H.ComponentDSL State Query Output m
