24c4f45e290aa8fdbe4d2da9852e3e4d1a73a9de
boygao1992 <wg2313@columbia.edu>
2018-10-21 21:10:46 -0400

    computational complexity: review

--- css/learn-css-layout/src/ViewComponent.purs
+++ css/learn-css-layout/src/ViewComponent.purs
@@ -622,6 +622,18 @@ percentWidth =
     [ HH.text "</article>"]
   ]
 
+-- | new spec, not popular enough
+column :: forall q. H.ComponentHTML q
+column =
+  HH.div [ classList [ ClassNames.elem, "three-column" ]]
+  [ HH.span [ classList [ ClassNames.label]]
+    [ HH.text "<div class=\"three-column\">"]
+  , HH.p_
+    [ HH.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit."]
+  , HH.span [ classList [ ClassNames.endLabel]]
+    [ HH.text "</div>"]
+  ]
+
 -- | Component
 render :: State -> H.ComponentHTML Query
 render _ =
